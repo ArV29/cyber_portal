b216ff8b890ed109673f1b633d848c1c6e5a3161
ArV <aryanrajvaidya@gmail.com>
2022-03-22 05:12:14 +0530

    New design and components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AllQuestions from "./Questions/AllQuestions"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import QuestionThread from "./Questions/QuestionThread"
 
 ReactDOM.render(
